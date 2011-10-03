af817594b0d2a782b1fa6eab433bbc00ae614c0b
blackforest <hakan.kocabas@gmx.de>
2011-10-03 20:52:08 +0200

    - Resourcebundle (internationalization)

--- idobjects-browser/src/main/java/com/idobjects/browser/MainApplication.java
+++ idobjects-browser/src/main/java/com/idobjects/browser/MainApplication.java
@@ -1,5 +1,10 @@
 package com.idobjects.browser;
 
+import java.awt.Font;
+import java.util.Locale;
+
+import javax.swing.JLabel;
+
 import org.apache.log4j.Logger;
 import org.jdesktop.application.Application;
 import org.jdesktop.application.SingleFrameApplication;
@@ -7,25 +12,28 @@ import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
-
 public class MainApplication extends SingleFrameApplication{
 
     private static final Logger LOGGER = Logger.getLogger( MainApplication.class );
-    
+
     @Override protected void initialize( String[] args ){
-        DateTime dt = new DateTime();
-        DateTimeFormatter fmt = DateTimeFormat.forPattern("dd-MM-yyyy HH:mm:ss");
-        
-        LOGGER.info( "Preparing start of IDOBJECTS-BROWSER at "+fmt.print( dt ) );
+        DateTimeFormatter fmt = DateTimeFormat.forPattern( "dd-MM-yyyy HH:mm:ss" );
+
+        LOGGER.info( "Preparing start of IDOBJECTS-BROWSER at " + fmt.print( new DateTime() ) );
+        LOGGER.info( "Default locale =" + Locale.getDefault() );
+
+        Locale.setDefault( Locale.GERMANY );
     }
-    
+
     @Override protected void startup(){
-        
+        // getMainFrame().setTitle( "BasicSingleFrameApp" );
+        JLabel label = new JLabel( "Hello, world!" );
+        label.setFont( new Font( "SansSerif", Font.PLAIN, 22 ) );
+        show( label );
     }
-    
-    
+
     public static void main( String[] args ){
         Application.launch( MainApplication.class, args );
     }
-    
+
 }
